fd738d531bf63429cb4d59cdecb7e23b1d30a23f
krosarioj <66438794+kevinrosario05@users.noreply.github.com>
2023-10-18 19:41:32 -0400

    subiendo

---
+++ ACardWeb/Keywords/LoginMethods.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrowserDriver;
+using ACardWeb.ObjectRepository;
+
+namespace ACardWeb.Keywords
+{
+    public class LoginMethods
+    {
+        public static void IngresarUsuario(WebDriver driver, string value)
+        {
+            if (CommonMethods.ValidateValue(value))
+            {
+                driver.FindElement(LoginObjects.input_Usuario).SendKeys(value);
+            }
+        }
+        public static void IngresarClave(WebDriver driver, string value)
+        {
+            if (CommonMethods.ValidateValue(value))
+            {
+                driver.FindElement(LoginObjects.input_Clave).SendKeys(value);
+            }
+        }
+        public static void PresionarEntrar(WebDriver driver)
+        {
+            driver.FindElement(LoginObjects.button_Entrar).Click();
+        }
+    }
+}
---
+++ ACardWeb/Tests/Login.cs
@@ -0,0 +1,33 @@
+﻿using ACardWeb.Keywords;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACardWeb.Tests
+{
+    [TestClass]
+    public class Login : BrowserDriver.DriverHelper
+    {
+        [TestMethod ("Iniciar sesion con las credenciales correctas")]
+        public void IniciarSesion_CredencialesCorrectas()
+        {
+            Iniciar("CHROME");
+            LoginMethods.IngresarUsuario(driver, Profiles.QA.Usuario);
+            LoginMethods.IngresarClave(driver, Profiles.QA.Clave);
+            LoginMethods.PresionarEntrar(driver);
+            //Validaciones
+        }
+        [TestMethod("Iniciar sesion con las credenciales incorrectas")]
+        public void IniciarSesion_CredencialesInorrectas()
+        {
+            Iniciar("CHROME");
+            LoginMethods.IngresarUsuario(driver, Profiles.QA.Usuario);
+            LoginMethods.IngresarClave(driver, "asasas");
+            LoginMethods.PresionarEntrar(driver);
+            //Validaciones
+        }
+    }
+}
